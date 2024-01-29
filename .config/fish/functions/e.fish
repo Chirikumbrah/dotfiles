c095e06547b6ef9cff366778ce316f19a4752774
Chirikumbrah <yarik.bond98@gmail.com>
2024-01-29 11:49:38 +0300

    simplified e.fish function

--- .config/fish/functions/e.fish
+++ .config/fish/functions/e.fish
@@ -1,26 +1,7 @@
 function e
     if count $argv >/dev/null
-        if test "$argv[1]" = "$HOME/" || test "$argv[1]" = "$HOME" || test "$argv[1]" = /
-            set FILE "$(fd . "$argv[1]" \
-            --hidden \
-            -I \
-            --exclude "*venv" \
-            --exclude ".git" \
-            -tf --color=always | \
-            fzf --ansi --preview '~/.config/fish/scripts/fzf-preview.sh {}')"
-        else
-            set FILE "$argv[1]"
-        end
+        $EDITOR $argv
     else
-        set FILE "$(fd \
-        --hidden \
-        -I \
-        --exclude "*venv" \
-         --exclude ".git" \
-        -tf --color=always | \
-        fzf --ansi --preview '~/.config/fish/scripts/fzf-preview.sh {}')"
-    end
-    if test $FILE != ''
-        $EDITOR "$FILE"
+        $EDITOR .
     end
 end
