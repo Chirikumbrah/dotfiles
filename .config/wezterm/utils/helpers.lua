8b98909a82f8576482273d5db9953716e9c7c243
Chirikumbrah <yarik.bond98@gmail.com>
2024-02-01 19:38:06 +0300

    some wezterm config changes

--- .config/wezterm/utils/helpers.lua
+++ .config/wezterm/utils/helpers.lua
@@ -16,4 +16,51 @@ M.get_random_entry = function(tbl)
 	return tbl[randomKey]
 end
 
+M.merge_lists = function(t1, t2, t3)
+	local result = {}
+	for _, v in pairs(t1) do
+		table.insert(result, v)
+	end
+	for _, v in pairs(t2) do
+		table.insert(result, v)
+	end
+	for _, v in pairs(t3) do
+		table.insert(result, v)
+	end
+	return result
+end
+
+local wt_action = require("wezterm").action
+
+M.multiple_actions = function(keys)
+	local actions = {}
+	for key in keys:gmatch(".") do
+		table.insert(actions, wt_action.SendKey({ key = key }))
+	end
+	table.insert(actions, wt_action.SendKey({ key = "\n" }))
+	return wt_action.Multiple(actions)
+end
+
+M.key_table = function(mods, key, action)
+	return {
+		mods = mods,
+		key = key,
+		action = action,
+	}
+end
+
+M.cmd_key = function(key, action)
+	return M.key_table("CMD", key, action)
+end
+
+M.cmd_to_tmux_prefix = function(key, tmux_key)
+	return M.cmd_key(
+		key,
+		wt_action.Multiple({
+			wt_action.SendKey({ mods = "CTRL", key = "b" }),
+			wt_action.SendKey({ key = tmux_key }),
+		})
+	)
+end
+
 return M
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,5 @@
---
--- ██╗    ██╗███████╗███████╗████████╗███████╗██████╗ ███╗   ███╗
 -- ██║    ██║██╔════╝╚══███╔╝╚══██╔══╝██╔════╝██╔══██╗████╗ ████║
+-- ██╗    ██╗███████╗███████╗████████╗███████╗██████╗ ███╗   ███╗
 -- ██║ █╗ ██║█████╗    ███╔╝    ██║   █████╗  ██████╔╝██╔████╔██║
 -- ██║███╗██║██╔══╝   ███╔╝     ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║
 -- ╚███╔███╔╝███████╗███████╗   ██║   ███████╗██║  ██║██║ ╚═╝ ██║
@@ -8,16 +7,17 @@
 -- A GPU-accelerated cross-platform terminal emulator
 -- https://wezfurlong.org/wezterm/
 
-local h = require("utils/helpers")
-local k = require("utils/keys")
-
 local wezterm = require("wezterm")
-local act = wezterm.action
-local mux = wezterm.mux
+local bindings = require("bindings")
 
 local config = {
+	default_prog = { '/usr/local/bin/fish',
+		'-l',
+		'-c',
+		'tmux attach || tmux new -s tmux'
+	},
 
-	line_height = 1.1,
+	line_height = 1,
 	font = wezterm.font 'JetBrainsMono Nerd Font',
 	font_rules = {
 		{
@@ -37,15 +37,15 @@ local config = {
 
 	-- window_background_opacity = 1.0,
 	window_padding = {
-		left = 30,
-		right = 30,
-		top = 20,
-		bottom = 10,
+		left = 5,
+		right = 5,
+		top = 5,
+		bottom = 0,
 	},
 
 	-- to start WezTerm maximized set theese two parameters
 	initial_cols = 200,
-	initial_rows = 50,
+	initial_rows = 60,
 
 	set_environment_variables = {
 		TERM = "xterm-256color",
@@ -53,47 +53,22 @@ local config = {
 	},
 
 	-- general options
-	adjust_window_size_when_changing_font_size = false,
-	disable_default_key_bindings = false,
+	disable_default_key_bindings = true,
 	debug_key_events = false,
-	default_workspace = "mux",
 	native_macos_fullscreen_mode = false,
-	scrollback_lines = 5000,
+	scrollback_lines = 10000,
+	enable_scroll_bar = false,
 	window_close_confirmation = "NeverPrompt",
 	window_decorations = "RESIZE",
+	enable_tab_bar = false,
+	-- tab_bar_at_bottom = true,
+	-- use_fancy_tab_bar = false,
 
 	-- cursor config
 	default_cursor_style = 'BlinkingBlock',
 	-- animation_fps = 1,
 	-- cursor_blink_ease_in = 'Constant',
 	-- cursor_blink_ease_out = 'Constant',
-
-	-- tab bar options
-	enable_tab_bar = true,
-	use_fancy_tab_bar = false,
-	tab_bar_at_bottom = true,
-	show_new_tab_button_in_tab_bar = false,
-	show_tab_index_in_tab_bar = false,
-	-- hide_tab_bar_if_only_one_tab = true,
-	tab_max_width = 11,
-	colors = {
-		tab_bar = {
-			active_tab = {
-				bg_color = '#282a36',
-				fg_color = '#bd93f9',
-				intensity = 'Bold',
-			},
-			inactive_tab = {
-				bg_color = '#282a36',
-				fg_color = '#6272a4',
-			},
-			inactive_tab_hover = {
-				bg_color = '#282a36',
-				fg_color = '#6272b9',
-				intensity = 'Bold',
-			},
-		},
-	},
 
 	-- dim inactive panes
 	inactive_pane_hsb = {
@@ -102,130 +77,7 @@ local config = {
 	},
 
 	-- keys
-	keys = {
-
-		-- convinient helix usage
-		{
-			mods = "CMD",
-			key = "s",
-			action = act.SendKey({ mods = "CTRL", key = "s" })
-		},
-		{
-			mods = "CMD",
-			key = "u",
-			action = act.SendKey({ mods = "CTRL", key = "u" })
-		},
-		{
-			mods = "CMD",
-			key = "d",
-			action = act.SendKey({ mods = "CTRL", key = "d" })
-		},
-		{
-			mods = "CMD|SHIFT",
-			key = "a",
-			action = act.SendKey({ mods = "CTRL|SHIFT", key = "a" })
-		},
-		{
-			mods = "CMD|SHIFT",
-			key = "x",
-			action = act.SendKey({ mods = "CTRL|SHIFT", key = "x" })
-		},
-
-		{
-			mods = "CMD|SHIFT",
-			key = "t",
-			action = act.ShowTabNavigator
-		},
-		{
-			mods = "CMD|SHIFT",
-			key = "w",
-			action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" })
-		},
-		-- split panes
-		{
-			mods = "CMD",
-			key = "e",
-			action = act.SplitVertical
-		},
-		{
-			mods = "CMD|SHIFT",
-			key = "e",
-			action = act.SplitHorizontal
-		},
-
-		-- switch between panes
-		{
-			mods = "CMD",
-			key = "LeftArrow",
-			action = act.ActivatePaneDirection("Left")
-		},
-		{
-			mods = "CMD",
-			key = "RightArrow",
-			action = act.ActivatePaneDirection("Right")
-		},
-		{
-			mods = "CMD",
-			key = "UpArrow",
-			action = act.ActivatePaneDirection("Up")
-		},
-		{
-			mods = "CMD",
-			key = "DownArrow",
-			action = act.ActivatePaneDirection("Down")
-		},
-
-		-- resize panes
-		{
-			mods = "CMD|SHIFT",
-			key = "LeftArrow",
-			action = act.AdjustPaneSize({ "Left", 5 })
-		},
-		{
-			mods = "CMD|SHIFT",
-			key = "RightArrow",
-			action = act.AdjustPaneSize({ "Right", 5 })
-		},
-		{
-			mods = "CMD|SHIFT",
-			key = "UpArrow",
-			action = act.AdjustPaneSize({ "Up", 2 })
-		},
-		{
-			mods = "CMD|SHIFT",
-			key = "DownArrow",
-			action = act.AdjustPaneSize({ "Down", 2 })
-		},
-
-	},
-}
-
-local process_icons = {
-	['docker'] = wezterm.nerdfonts.linux_docker,
-	['docker-compose'] = wezterm.nerdfonts.linux_docker,
-	['psql'] = '󱤢',
-	['usql'] = '󱤢',
-	['kuberlr'] = wezterm.nerdfonts.linux_docker,
-	['kubectl'] = wezterm.nerdfonts.linux_docker,
-	['stern'] = wezterm.nerdfonts.linux_docker,
-	['nvim'] = wezterm.nerdfonts.custom_vim,
-	['make'] = wezterm.nerdfonts.seti_makefile,
-	['vim'] = wezterm.nerdfonts.dev_vim,
-	['node'] = wezterm.nerdfonts.mdi_hexagon,
-	['go'] = wezterm.nerdfonts.seti_go,
-	['zsh'] = wezterm.nerdfonts.dev_terminal,
-	['bash'] = wezterm.nerdfonts.cod_terminal_bash,
-	['btm'] = wezterm.nerdfonts.mdi_chart_donut_variant,
-	['htop'] = wezterm.nerdfonts.mdi_chart_donut_variant,
-	['cargo'] = wezterm.nerdfonts.dev_rust,
-	['sudo'] = wezterm.nerdfonts.fa_hashtag,
-	['lazydocker'] = wezterm.nerdfonts.linux_docker,
-	['git'] = wezterm.nerdfonts.dev_git,
-	['lua'] = wezterm.nerdfonts.seti_lua,
-	['wget'] = wezterm.nerdfonts.mdi_arrow_down_box,
-	['curl'] = wezterm.nerdfonts.mdi_flattr,
-	['gh'] = wezterm.nerdfonts.dev_github_badge,
-	['ruby'] = wezterm.nerdfonts.cod_ruby,
+	keys = bindings.create_keybinds(),
 }
 
 return config
