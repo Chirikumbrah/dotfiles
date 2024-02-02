2e98bd6eae93ef243f62a355e9b4584ef852dd46
Chirikumbrah <yarik.bond98@gmail.com>
2024-02-02 10:17:24 +0300

    changed gco func

--- .config/fish/functions/gco.fish
+++ .config/fish/functions/gco.fish
@@ -1,3 +1,3 @@
-function gco --wraps='git checkout $argv || git checkout -b $argv' --description 'alias gco=git checkout $argv || git checkout -b $argv'
-  git checkout $argv || git checkout -b $argv; 
+function gco --wraps='git checkout $argv' --description 'alias gco=git checkout $argv'
+  git checkout $argv 
 end
