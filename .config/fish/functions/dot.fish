441d921cb49e3554e64debb164a097883ecbae96
Chirikumbrah <yarik.bond98@gmail.com>
2024-02-05 10:34:12 +0300

    removed unused fish functions

---
+++ .config/fish/functions/dot.fish
@@ -0,0 +1,4 @@
+function dot --wraps='cd ~/Projects/mine/dotfiles' --description 'alias dot=cd ~/Projects/mine/dotfiles'
+  cd ~/Projects/mine/dotfiles $argv
+        
+end
--- .config/fish/functions/get_key.fish
+++
@@ -1,4 +0,0 @@
-function get_key --wraps='command cat -v' --description 'alias get_key=command cat -v'
-  command cat -v $argv
-        
-end
