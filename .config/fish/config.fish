5ff8b8955f86cf6db0f3c8ebb1bcd001603cc489
Chirikumbrah <yarik.bond98@gmail.com>
2024-02-09 16:39:12 +0300

    added scripts to PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -28,7 +28,7 @@ set -gx VISUAL $EDITOR
 set -U SHELL /usr/local/bin/fish
 
 # Set PATH
-set --universal fish_user_paths $fish_user_paths /opt/homebrew/bin "/Applications/Postgres.app/Contents/Versions/latest/bin" "/Library/Frameworks/Python.framework/Versions/3.12/bin" "~/.config/scripts"
+set --universal fish_user_paths $fish_user_paths /opt/homebrew/bin "/Applications/Postgres.app/Contents/Versions/latest/bin" "/Library/Frameworks/Python.framework/Versions/3.12/bin" "$HOME/.config/scripts"
 
 # Initioalize other apps
 eval "$(/opt/homebrew/bin/brew shellenv)"
