cddb3a150859d6aa7ac36905aaeeb23d5590aee8
Chirikumbrah <yarik.bond98@gmail.com>
2024-02-26 10:44:08 +0300

    nvim: disabled lazy and autopairs

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,7 +2,7 @@ return {
   { "folke/noice.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
   { "folke/flash.nvim", enabled = false },
-  -- { "echasnovski/mini.pairs", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
 
   -- temporarily disabled due to empty file issue
   -- { "nvim-navic", enabled = false },
