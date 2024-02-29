421933d64d086741f90d30491813e871e0ccc8fd
Chirikumbrah <yarik.bond98@gmail.com>
2024-02-29 13:36:32 +0300

    nvim: added whitespace chars render

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.wrap = true -- Enable line wrap
+local space = "·"
+vim.opt.listchars:append({
+  -- tab = "⇥", -- this one conflicts with plugins for indent line
+  eol = "↵",
+  multispace = space,
+  lead = space,
+  trail = space,
+  nbsp = space,
+})
