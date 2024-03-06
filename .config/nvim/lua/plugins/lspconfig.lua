0bc47afa181eb01ab07f6b1e936ba988a22fa89f
Chirikumbrah <yarik.bond98@gmail.com>
2024-03-06 10:46:24 +0300

    nvim: removed unused config lines

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,35 +1,5 @@
 return {
   {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "bash-language-server",
-        "docker-compose-language-service",
-        "dockerfile-language-server",
-        "hadolint",
-        "helm-ls",
-        "html-lsp",
-        "json-lsp",
-        "lua-language-server",
-        "marksman",
-        "markdownlint",
-        "prettier",
-        "ruff",
-        "ruff-lsp",
-        "shfmt",
-        "shellcheck",
-        "sql-formatter",
-        "sqlls",
-        "stylua",
-        "taplo",
-        "tflint",
-        "typescript-language-server",
-        "xmlformatter",
-        "yaml-language-server",
-      },
-    },
-  },
-  {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
     opts = {
