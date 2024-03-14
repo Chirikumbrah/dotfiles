162dba126da73b1016b0f543acd062bc82bf58f0
Chirikumbrah <yarik.bond98@gmail.com>
2024-03-14 15:27:25 +0300

    nvim: fix: treesitter yaml indent

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -3,6 +3,7 @@ return {
   { "rcarriga/nvim-notify", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "RRethy/vim-illuminate", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
   -- { "echasnovski/mini.pairs", enabled = false },
 
   -- temporarily disabled due to empty file issue
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    if type(opts.ensure_installed) == "table" then
-      vim.list_extend(opts.indent, { disable = { "yaml" } })
-    end
-  end,
+  opts = {
+    indent = { disable = { "yaml" } },
+  },
 }
