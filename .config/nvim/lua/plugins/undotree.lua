408700cc652f4819e998f6a9bb09883b2a23e994
Chirikumbrah <yarik.bond98@gmail.com>
2024-06-23 09:47:54 +0300

    applied some formatting

--- .config/nvim/lua/plugins/undotree.lua
+++ .config/nvim/lua/plugins/undotree.lua
@@ -1,9 +1,9 @@
 return {
-	"jiaoshijie/undotree",
-	dependencies = "nvim-lua/plenary.nvim",
-	config = true,
-	keys = {
-		-- load the plugin only when using it's keybinding:
-		{ "<leader>u", function() require('undotree').toggle() end },
-	},
+    "jiaoshijie/undotree",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = true,
+    keys = {
+        -- load the plugin only when using it's keybinding:
+        { "<leader>u", function() require('undotree').toggle() end },
+    },
 }
--- .config/nvim/lua/yr/keymaps.lua
+++ .config/nvim/lua/yr/keymaps.lua
@@ -1,6 +1,3 @@
--- Set the Leader Key
-vim.g.mapleader = " "
-
 -- Open Netrw Explorer
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
