9e480376a6b0a70bbf351ec83c297790e4827ef1
Chirikumbrah <yarik.bond98@gmail.com>
2024-06-26 08:11:12 +0300

    alacritty theme changes

--- .config/alacritty/scripts/change_theme.sh
+++ .config/alacritty/scripts/change_theme.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-IS_LIGHT=$(grep -q solarized_light ~/.config/alacritty/alacritty.toml ; echo $?)
-
-echo $IS_LIGHT
+THEME=gruvbox
+IS_LIGHT=$(grep -q ${THEME}_light ~/.config/alacritty/alacritty.toml ; echo $?)
 
 if [ "$IS_LIGHT" == 0 ]; then
-	sed -i "" "s/solarized_light/solarized_dark/" ~/.config/alacritty/alacritty.toml
+	sed -i "" "s/${THEME}_light/${THEME}_dark/" ~/.config/alacritty/alacritty.toml
 else
-	sed -i "" "s/solarized_dark/solarized_light/" ~/.config/alacritty/alacritty.toml
+	sed -i "" "s/${THEME}_dark/${THEME}_light/" ~/.config/alacritty/alacritty.toml
 fi
