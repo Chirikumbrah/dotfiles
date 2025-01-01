a7c5e03adc9b42417d57924b1252e6467bca8d8f
Chirikumbrah <dvps4k@gmail.com>
2025-01-01 11:56:46 +0300

    NeoVim: added "requirements" to TreeSitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -15,6 +15,7 @@ return {
                 "lua",
                 "markdown",
                 "python",
+                "requirements",
                 "toml",
                 "yaml",
             },
