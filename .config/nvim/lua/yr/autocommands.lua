f3f7980e3319dab213e1d8111827aab0d8aa8fe6
Chirikumbrah <dvps4k@gmail.com>
2025-01-03 13:34:41 +0300

    NeoVim: added autocmd to open file on last position

--- .config/nvim/lua/yr/autocommands.lua
+++ .config/nvim/lua/yr/autocommands.lua
@@ -5,9 +5,9 @@ local function augroup(name)
 end
 
 -- Detect Helm templates as helm filetype
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
-    pattern = {"*/templates/*.y*ml", "*/templates/*.tpl"},
-    command = "set filetype=helm"
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = { "*/templates/*.y*ml", "*/templates/*.tpl" },
+    command = "set filetype=helm",
 })
 -- Define syntax and indentation for helm filetype
 vim.api.nvim_create_augroup("helm_syntax", { clear = true })
@@ -17,7 +17,7 @@ vim.api.nvim_create_autocmd("FileType", {
     callback = function()
         vim.opt_local.syntax = "yaml"
     end,
-    group = "helm_syntax"
+    group = "helm_syntax",
 })
 
 -- Remove Trailing whitespace
@@ -26,34 +26,11 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     command = [[%s/\s\+$//e]],
 })
 
--- -- Function to check if a floating dialog exists and if not
--- -- then check for diagnostics under the cursor
--- function OpenDiagnosticIfNoFloat()
---     for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
---         if vim.api.nvim_win_get_config(winid).zindex then
---             return
---         end
---     end
---     -- THIS IS FOR BUILTIN LSP
---     vim.diagnostic.open_float(0, {
---         scope = "cursor",
---         focusable = false,
---         close_events = {
---             "CursorMoved",
---             "CursorMovedI",
---             "BufHidden",
---             "InsertCharPre",
---             "WinLeave",
---         },
---     })
--- end
--- -- Show diagnostics under the cursor when holding position
--- vim.api.nvim_create_augroup("lsp_diagnostics_hold", { clear = true })
--- vim.api.nvim_create_autocmd({ "CursorHold" }, {
---     pattern = "*",
---     command = "lua OpenDiagnosticIfNoFloat()",
---     group = "lsp_diagnostics_hold",
--- })
+-- Restore cursor position
+vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+    pattern = { "*" },
+    command = 'silent! normal! g`"zv',
+})
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
