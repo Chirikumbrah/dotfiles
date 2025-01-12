fb096b3aaa9b1b0836118152c106c13508ec096f
Chirikumbrah <dvps4k@gmail.com>
2025-01-12 10:25:52 +0300

    NeoVim: set cmp window on keypress

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,63 +1,32 @@
 return {
-    {
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
-        dependencies = {
-            "L3MON4D3/LuaSnip",
-            "saadparwaiz1/cmp_luasnip",
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "felipelema/cmp-async-path",
-        },
-        config = function()
-            local luasnip = require("luasnip")
-            local cmp = require("cmp")
-
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<CR>"] = cmp.mapping.confirm({
-                        behavior = cmp.ConfirmBehavior.Replace,
-                        select = true,
-                    }),
-                    ["<C-n>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif luasnip.expand_or_jumpable() then
-                            luasnip.expand_or_jump()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                    ["<C-p>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif luasnip.jumpable(-1) then
-                            luasnip.jump(-1)
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                }),
-                sources = {
-                    { name = "nvim_lsp", max_item_count = 5 },
-                    { name = "buffer", max_item_count = 5 },
-                    { name = "async_path", max_item_count = 3 },
-                    { name = "luasnip", max_item_count = 3 },
-                    { name = "vsnip", max_item_count = 3 },
-                },
-                formatting = {
-                    format = function(_, vim_item)
-                        vim_item.abbr = string.sub(vim_item.abbr, 1, 20)
-                        return vim_item
-                    end,
-                },
-            })
-        end,
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "hrsh7th/cmp-nvim-lsp",
     },
+    config = function()
+        local cmp = require("cmp")
+        cmp.setup({
+            completion = { autocomplete = false, },
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-e>"] = function(fallback) if cmp.visible() then cmp.close() else cmp.complete() end end,
+                ["<C-d>"] = cmp.mapping.scroll_docs(4),
+                ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-n>"] = function(fallback) if cmp.visible() then cmp.select_next_item() else fallback() end end,
+                ["<C-p>"] = function(fallback) if cmp.visible() then cmp.select_prev_item() else fallback() end end,
+            }),
+            sources = {
+                { name = "nvim_lsp", max_item_count = 5 },
+                { name = "luasnip", max_item_count = 5 },
+                { name = "vsnip", max_item_count = 5 },
+            },
+        })
+    end,
 }
