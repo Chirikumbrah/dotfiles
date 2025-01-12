74776ea314046975da3a556a22b370875daa32c3
Chirikumbrah <dvps4k@gmail.com>
2025-01-12 12:51:19 +0300

    NeoVim: moved cmp config to lsp config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,8 +4,22 @@ return {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
         "WhoIsSethDaniel/mason-tool-installer.nvim",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
     },
     config = function()
+        -- variables
+        local cmp = require('cmp')
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities())
+        -- lsp autoinstall
         require("mason-tool-installer").setup({
             ensure_installed = {
                 "bash-language-server",
@@ -24,14 +38,36 @@ return {
                 "yaml-language-server",
             },
         })
+        -- lsp configs
         require("mason").setup()
-        require("mason-lspconfig").setup()
-        require("mason-lspconfig").setup_handlers({
-            function(server_name)
-                require("lspconfig")[server_name].setup({
-                    -- capabilities = require("cmp_nvim_lsp").default_capabilities(),
-                })
-            end,
+        require("mason-lspconfig").setup({
+            handlers = {
+                function(server_name)
+                    require("lspconfig")[server_name].setup({
+                        capabilities = capabilities
+                    })
+                end,
+            }
+        })
+        -- completion configs
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-e>"] = function(fallback) if cmp.visible() then cmp.close() else cmp.complete() end end,
+                ["<C-d>"] = cmp.mapping.scroll_docs(4),
+                ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+                ['<C-p>'] = cmp.mapping.select_prev_item(),
+                ['<C-n>'] = cmp.mapping.select_next_item(),
+            }),
+            sources = {
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+                { name = "buffer" },
+            },
         })
     end,
 }
