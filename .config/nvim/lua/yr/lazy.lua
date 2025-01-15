72ca6797c7e88612c62b00d730cb4e6eb0a5dcf1
Chirikumbrah <dvps4k@gmail.com>
2025-01-15 11:07:47 +0300

    NeoVim: restructured config

--- .config/nvim/lua/yr/lazy.lua
+++ .config/nvim/lua/yr/lazy.lua
@@ -1,20 +1,21 @@
--- Setting up options for lazy.nvim
-local opts = {
-    change_detection = {
-        -- automatically check for config file changes and reload the ui
-        enabled = true,
-        notify = false, -- get a notification when changes are found
-    },
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    spec = "yr.plugins",
+    change_detection = { notify = false },
     performance = {
-        cache = {
-            enabled = true,
-        },
-        reset_packpath = true, -- reset the package path to improve startup time
         rtp = {
-            reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
-            ---@type string[]
-            paths = {}, -- add any custom paths here that you want to includes in the rtp
-            ---@type string[] list any plugins you want to disable here
             disabled_plugins = {
                 "gzip",
                 "tarPlugin",
@@ -24,6 +25,4 @@ local opts = {
             },
         },
     },
-}
-
-require("lazy").setup("plugins", opts)
+})
