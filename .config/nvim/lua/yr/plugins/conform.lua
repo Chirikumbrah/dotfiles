0df954e2210f2e7d298fac5015bf3efa43a8cb36
Chirikumbrah <dvps4k@gmail.com>
2025-01-18 20:09:19 +0300

    NeoVim: separated conform keymaps

--- .config/nvim/lua/yr/plugins/conform.lua
+++ .config/nvim/lua/yr/plugins/conform.lua
@@ -1,35 +1,42 @@
 return {
     "stevearc/conform.nvim",
-    opts = {},
     event = { "BufWritePre" },
     cmd = { "ConformInfo" },
-    config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-                css = { "prettier" },
-                go = { "goimports", "gofmt" },
-                graphql = { "prettier" },
-                javascript = { "prettier" },
-                javascriptreact = { "prettier" },
-                json = { "prettier" },
-                lua = { "stylua" },
-                markdown = { "prettier" },
-                python = { "ruff-lsp" },
-                sh = { "beautysh" },
-                zsh = { "beautysh" },
-                toml = { "taplo" },
-                typescript = { "prettier" },
-                typescriptreact = { "prettier" },
-                yaml = { "prettier" },
+    opts = {
+        formatters_by_ft = {
+            css = { "prettier" },
+            go = { "goimports", "gofmt" },
+            graphql = { "prettier" },
+            javascript = { "prettier" },
+            javascriptreact = { "prettier" },
+            json = { "prettier" },
+            lua = { "stylua" },
+            markdown = { "prettier" },
+            python = { "ruff-lsp" },
+            sh = { "beautysh" },
+            zsh = { "beautysh" },
+            toml = { "taplo" },
+            typescript = { "prettier" },
+            typescriptreact = { "prettier" },
+            yaml = { "prettier" },
+        },
+        formatters = {
+            shfmt = {
+                prepend_args = { "-i", "4", "-ci" },
             },
-            formatters = {
-                shfmt = {
-                    prepend_args = { "-i", "4", "-ci" },
-                },
-                stylua = {
-                    prepend_args = { "--indent-type", "Spaces" },
-                },
+            stylua = {
+                prepend_args = { "--indent-type", "Spaces" },
             },
-        })
-    end,
+        },
+    },
+    keys = {
+        {
+            "<leader>=",
+            function()
+                require("conform").format({ async = true, lsp_format = "fallback" })
+            end,
+            mode = "n",
+            desc = "Format buffer",
+        },
+    },
 }
