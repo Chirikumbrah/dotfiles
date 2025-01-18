3c59d05a72616636aa02ccc3facbbfb71732a4b7
Chirikumbrah <dvps4k@gmail.com>
2025-01-18 20:08:04 +0300

    NeoVim: split plugins from other.lua

--- .config/nvim/lua/yr/plugins/other.lua
+++
@@ -1,6 +0,0 @@
-return {
-    { "leath-dub/snipe.nvim", opts = {} },
-    { "kylechui/nvim-surround", event = "VeryLazy", opts = {} },
-    { "lewis6991/gitsigns.nvim", event = "VeryLazy", opts = {} },
-    { "ibhagwan/fzf-lua", opts = {} },
-}
