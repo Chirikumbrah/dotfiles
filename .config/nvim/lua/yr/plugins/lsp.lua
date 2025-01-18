2cf0597caea78bad5da6707d7744fc6db59fa7d1
Chirikumbrah <dvps4k@gmail.com>
2025-01-18 15:01:44 +0300

    NeoVim: simplified LSP config

--- .config/nvim/lua/yr/plugins/lsp.lua
+++ .config/nvim/lua/yr/plugins/lsp.lua
@@ -3,7 +3,6 @@ return {
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
         "hrsh7th/nvim-cmp",
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
@@ -12,45 +11,24 @@ return {
     },
     config = function()
         vim.diagnostic.config({ virtual_text = false, severity_sort = true })
-        -- variables
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
-        -- lsp autoinstall
-        require("mason-tool-installer").setup({
-            ensure_installed = {
-                "bash-language-server",
-                "clangd",
-                "docker-compose-language-service",
-                "dockerfile-language-server",
-                "gofumpt",
-                "goimports",
-                "gopls",
-                "lua-language-server",
-                "prettier",
-                "pyright",
-                "ruff",
-                "beautysh",
-                "stylua",
-                "yaml-language-server",
-            },
-        })
         -- lsp configs
         require("mason").setup()
         require("mason-lspconfig").setup({
             handlers = {
                 function(server_name)
                     require("lspconfig")[server_name].setup({
-                        capabilities = capabilities
+                        capabilities = vim.tbl_deep_extend(
+                            "force",
+                            {},
+                            vim.lsp.protocol.make_client_capabilities(),
+                            require("cmp_nvim_lsp").default_capabilities()
+                        ),
                     })
                 end,
-            }
+            },
         })
         -- completion configs
+        local cmp = require("cmp")
         cmp.setup({
             completion = { autocomplete = false },
             snippet = {
@@ -59,11 +37,8 @@ return {
                 end,
             },
             mapping = cmp.mapping.preset.insert({
-                ["<C-k>"] = function(fallback) if cmp.visible() then cmp.close() else cmp.complete() end end,
                 ["<C-d>"] = cmp.mapping.scroll_docs(4),
                 ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-n>"] = function(fallback) if cmp.visible() then cmp.select_next_item() else fallback() end end,
-                ["<C-p>"] = function(fallback) if cmp.visible() then cmp.select_prev_item() else fallback() end end,
             }),
             sources = {
                 { name = "nvim_lsp" },
