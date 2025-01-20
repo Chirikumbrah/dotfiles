c081e922bb2f929656d8bd8a507eb7b04616dd2a
Chirikumbrah <dvps4k@gmail.com>
2025-01-20 17:24:29 +0300

    NeoVim: optimized lazy-load for LSP

--- .config/nvim/lua/yr/plugins/lsp.lua
+++ .config/nvim/lua/yr/plugins/lsp.lua
@@ -1,19 +1,10 @@
 return {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPost", "BufNewFile" },
-    cmd = { "LspInfo", "LspInstall", "LspUninstall" },
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
+    {
         "WhoIsSethDaniel/mason-tool-installer.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-    },
-    config = function()
-        vim.diagnostic.config({ virtual_text = false, severity_sort = true })
-        -- lsp configs
-        require("mason").setup()
-        require("mason-lspconfig").setup()
-        require("mason-tool-installer").setup({
+        lazy = true,
+        dependencies = { "williamboman/mason.nvim" },
+        cmd = "MasonToolsInstallSync",
+        opts = {
             ensure_installed = {
                 "bash-language-server",
                 "clangd",
@@ -30,16 +21,42 @@ return {
                 "stylua",
                 "yaml-language-server",
             },
-        })
-        -- keymaps
-        vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover, { desc = "Show docs for item under cursor" })
-        vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, { desc = "Perform code action" })
-        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename symbol" })
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
-        vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, { desc = "Go to type definition" })
-    end,
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        event = "VeryLazy",
+        cmd = { "LspInfo", "LspInstall", "LspUninstall" },
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
+        },
+        config = function()
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                handlers = {
+                    function(server_name)
+                        require("lspconfig")[server_name].setup({
+                            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+                        })
+                    end,
+                },
+            })
+            vim.diagnostic.config({ virtual_text = false, severity_sort = true })
+            vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover, { desc = "Show docs for item under cursor" })
+            vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, { desc = "Perform code action" })
+            vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename symbol" })
+            vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
+            vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+            vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, { desc = "Go to type definition" })
+            -- start lsp on VeryLazy event
+            vim.cmd("MasonToolsInstallSync")
+            vim.cmd("LspStart")
+        end,
+    },
 }
