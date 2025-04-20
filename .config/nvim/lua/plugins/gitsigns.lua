bb1e88748799cfd5e0cf71d05157e1edecb36658
Chirikumbrah <dvps4k@gmail.com>
2025-04-20 23:07:15 +0300

    NeoVim: clarified descriptions for Gitsigns keymaps

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -3,10 +3,10 @@ return {
     event = "VeryLazy",
     cmd = "Gitsigns",
     keys = {
-        { "]h", function() require("gitsigns").next_hunk() end, desc = "Go to next git hunk" },
-        { "[h", function() require("gitsigns").prev_hunk() end, desc = "Go to previous git hunk" },
-        { "<leader>h", function() require("gitsigns").stage_hunk() end, desc = "Stage hunk", mode = { "n", "v" } },
-        { "<leader>l", function() require("gitsigns").toggle_current_line_blame() end, desc = "Toggle current line blame" },
+        { "]h", function() require("gitsigns").next_hunk() end, desc = "Go to next git hunk [gitsigns]" },
+        { "[h", function() require("gitsigns").prev_hunk() end, desc = "Go to previous git hunk [gitsigns]" },
+        { "<leader>h", function() require("gitsigns").stage_hunk() end, desc = "Stage hunk [gitsigns]", mode = { "n", "v" } },
+        { "<leader>l", function() require("gitsigns").toggle_current_line_blame() end, desc = "Toggle current line blame [gitsigns]" },
     },
     opts = {},
 }
