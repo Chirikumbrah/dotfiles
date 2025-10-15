6ffff42ed77b8eb0b5d821715ab94bcf895a2fcb
Chirikumbrah <dvps4k@gmail.com>
2025-10-15 13:24:17 +0300

    NeoVim: added some LSPs

--- .config/nvim/lsp/yamlls.lua
+++ .config/nvim/lsp/yamlls.lua
@@ -1,9 +1,19 @@
 return {
-    cmd = { "yaml-language-server", "--stdio" },
-    filetypes = { "yaml", "yaml.docker-compose", "yaml.gitlab" },
-    root_markers = { ".git" },
-    settings = {
-        -- https://github.com/redhat-developer/vscode-redhat-telemetry#how-to-disable-telemetry-reporting
-        redhat = { telemetry = { enabled = false } },
+  cmd = { "yaml-language-server", "--stdio" },
+  filetypes = { "yaml", "yaml.docker-compose", "yaml.gitlab" },
+  root_markers = { ".git" },
+  settings = {
+    -- https://github.com/redhat-developer/vscode-redhat-telemetry#how-to-disable-telemetry-reporting
+    redhat = { telemetry = { enabled = false } },
+    yaml = {
+      format = { enable = true, printWidth = 100 }, -- enable formatting support
+      validate = true,
     },
+  },
+  on_attach = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
+  end,
 }
