ee7e4cb8e13c06f68b97db7cfe2f442ade2edc06
Chirikumbrah <dvps4k@gmail.com>
2025-11-08 02:55:17 +0300

    NeoVim: formatted init.lua; added test autocmd to restore cursor position

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,7 @@
 -- HELPERS {{{
 vim.g.mapleader = " "
 local function km(mode, key, func, desc)
-  vim.keymap.set(mode, key, func, { desc = desc, silent = true })
+    vim.keymap.set(mode, key, func, { desc = desc, silent = true })
 end
 -- }}}
 
@@ -42,14 +42,14 @@ vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#3f3f3f", bg = "none" })
 local path_package = vim.fn.stdpath("data") .. "/site/"
 local mini_path = path_package .. "pack/deps/start/mini.nvim"
 if not vim.loop.fs_stat(mini_path) then
-  vim.cmd('echo "Installing `mini.nvim`" | redraw')
-  -- stylua: ignore start
-  local clone_cmd = { "git", "clone", "--filter=blob:none",
-    "https://github.com/echasnovski/mini.nvim", mini_path }
-  -- stylua: ignore end
-  vim.fn.system(clone_cmd)
-  vim.cmd("packadd mini.nvim | helptags ALL")
-  vim.cmd('echo "Installed `mini.nvim`" | redraw')
+    vim.cmd('echo "Installing `mini.nvim`" | redraw')
+    -- stylua: ignore start
+    local clone_cmd = { "git", "clone", "--filter=blob:none",
+        "https://github.com/echasnovski/mini.nvim", mini_path }
+    -- stylua: ignore end
+    vim.fn.system(clone_cmd)
+    vim.cmd("packadd mini.nvim | helptags ALL")
+    vim.cmd('echo "Installed `mini.nvim`" | redraw')
 end
 
 -- Set up 'mini.deps' (customize to your liking)
@@ -61,173 +61,186 @@ local add, later, now = MiniDeps.add, MiniDeps.later, MiniDeps.now
 
 -- EXPLICIT LOAD {{{
 now(function()
-  -- AUTOCOMMANDS {{{
-  local group = vim.api.nvim_create_augroup("own", { clear = true })
+    -- AUTOCOMMANDS {{{
+    local group = vim.api.nvim_create_augroup("own", { clear = true })
 
-  vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-    group = group,
-    pattern = { "*/templates/*.y*ml", "*/templates/*.tpl", "Chart.y*ml" },
-    command = "set filetype=helm",
-  })
+    vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+        group = group,
+        pattern = { "*/templates/*.y*ml", "*/templates/*.tpl", "Chart.y*ml" },
+        command = "set filetype=helm",
+    })
 
-  vim.api.nvim_create_autocmd("TextYankPost", { -- Highlight on yank
-    group = group,
-    callback = function()
-      vim.highlight.on_yank({ higroup = "Visual", timeout = 400 })
-    end,
-  })
+    vim.api.nvim_create_autocmd("TextYankPost", { -- Highlight on yank
+        group = group,
+        callback = function()
+            vim.highlight.on_yank({ timeout = 400, visual = true })
+        end,
+    })
 
-  vim.api.nvim_create_autocmd("FileType", { -- close quickfix/locations list
-    group = group,
-    pattern = { "qf", "help" },
-    callback = function()
-      local wi = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1]
-      local close
-      if vim.bo.filetype == "help" then
-        close = ":q<CR>" -- close help window
-      else
-        close = wi.loclist == 1 and ":lclose<CR>" or ":cclose<CR>"
-      end
-      km("n", "<CR>", "<CR>" .. close, "") -- <CR>: select -> close
-      km("n", "q", close, "")              -- q and <Esc>: just close
-      km("n", "<Esc>", close, "")
-    end,
-  })
+    vim.api.nvim_create_autocmd("BufReadPost", { -- restore cursor last position
+        callback = function(args)
+            local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+            local line_count = vim.api.nvim_buf_line_count(args.buf)
+            if mark[1] > 0 and mark[1] <= line_count then
+                vim.api.nvim_win_set_cursor(0, mark)
+                vim.schedule(function()
+                    vim.cmd("normal! zz")
+                end)
+            end
+        end,
+    })
 
-  vim.api.nvim_create_autocmd("LspAttach", {
-    group = group,
-    callback = function(args)
-      local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-      if client:supports_method("textDocument/completion") then
-        -- Optional: trigger autocompletion on EVERY keypress. May be slow!
-        local chars = {}
-        for i = 32, 126 do
-          table.insert(chars, string.char(i))
-        end
-        -- stylua: ignore start
-        vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-        vim.opt.completeopt:append({ "menuone", "noselect", "popup", "fuzzy" })
-        km("i", "<C-Space>", function() vim.lsp.completion.get() end)
-        -- stylua: ignore end
-      end
-    end,
-  })
-  -- }}}
+    vim.api.nvim_create_autocmd("FileType", { -- close quickfix/locations list
+        group = group,
+        pattern = { "qf", "help" },
+        callback = function()
+            local wi = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1]
+            local close
+            if vim.bo.filetype == "help" then
+                close = ":q<CR>" -- close help window
+            else
+                close = wi.loclist == 1 and ":lclose<CR>" or ":cclose<CR>"
+            end
+            km("n", "<CR>", "<CR>" .. close, "") -- <CR>: select -> close
+            km("n", "q", close, "")              -- q and <Esc>: just close
+            km("n", "<Esc>", close, "")
+        end,
+    })
 
-  -- LSP {{{
-  -- stylua: ignore start
-  vim.lsp.enable({
-    "luals", "clangd", "gopls", "ruff", "pyright", "bashls", "dockerls", "terraformls",
-    "helmls", "yamlls", "jsonls",
-  })
-  -- stylua: ignore end
-  vim.diagnostic.config({ virtual_text = false, severity_sort = true })
-  -- }}}
+    vim.api.nvim_create_autocmd("LspAttach", {
+        group = group,
+        callback = function(args)
+            local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+            if client:supports_method("textDocument/completion") then
+                -- Optional: trigger autocompletion on EVERY keypress. May be slow!
+                local chars = {}
+                for i = 32, 126 do
+                    table.insert(chars, string.char(i))
+                end
+                -- stylua: ignore start
+                vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+                vim.opt.completeopt:append({ "menuone", "noselect", "popup", "fuzzy" })
+                km("i", "<C-Space>", function() vim.lsp.completion.get() end)
+                -- stylua: ignore end
+            end
+        end,
+    })
+    -- }}}
 
-  add({
-    source = "nvim-treesitter/nvim-treesitter",
-    hooks = {
-      post_checkout = function()
-        vim.cmd("TSUpdate")
-      end,
-    },
-  })
-  require("nvim-treesitter.configs").setup({
-    auto_install = true,
-    highlight = {
-      enable = true,
-      ---@diagnostic disable-next-line: unused-local
-      disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local buf_name = vim.api.nvim_buf_get_name(buf)
-        local ok, stats = pcall(vim.loop.fs_stat, buf_name)
-        if ok and stats and stats.size > max_filesize then
-          return true
-        end
-      end,
-    },
-  })
+    -- LSP {{{
+    -- stylua: ignore start
+    vim.lsp.enable({
+        "luals", "clangd", "gopls", "ruff", "pyright", "bashls", "dockerls", "terraformls",
+        "helmls", "yamlls", "jsonls",
+    })
+    -- stylua: ignore end
+    vim.diagnostic.config({ virtual_text = false, severity_sort = true })
+    -- }}}
+
+    add({
+        source = "nvim-treesitter/nvim-treesitter",
+        hooks = {
+            post_checkout = function()
+                vim.cmd("TSUpdate")
+            end,
+        },
+    })
+    require("nvim-treesitter.configs").setup({
+        auto_install = true,
+        highlight = {
+            enable = true,
+            ---@diagnostic disable-next-line: unused-local
+            disable = function(lang, buf)
+                local max_filesize = 100 * 1024 -- 100 KB
+                local buf_name = vim.api.nvim_buf_get_name(buf)
+                local ok, stats = pcall(vim.loop.fs_stat, buf_name)
+                if ok and stats and stats.size > max_filesize then
+                    return true
+                end
+            end,
+        },
+    })
 end)
 -- }}}
 
 -- LAZY LOAD {{{
 later(function()
-  -- PLUGINS {{{
-  -- stylua: ignore start
-  -- add({ source = "stevearc/conform.nvim" })
-  add({ source = "olexsmir/gopher.nvim" })
-  add({ source = "folke/zen-mode.nvim" })
-  add({ source = "ibhagwan/fzf-lua" })
-  add({ source = "folke/which-key.nvim" })
-  -- stylua: ignore end
+    -- PLUGINS {{{
+    -- stylua: ignore start
+    -- add({ source = "stevearc/conform.nvim" })
+    add({ source = "olexsmir/gopher.nvim" })
+    add({ source = "folke/zen-mode.nvim" })
+    add({ source = "ibhagwan/fzf-lua" })
+    add({ source = "folke/which-key.nvim" })
+    -- stylua: ignore end
 
-  -- stylua: ignore start
+    -- stylua: ignore start
 
-  require("which-key").setup({
-    preset = "helix",
-    icons = { mappings = false },
-    spec = { { "gr", group = "+LSP/fzf-lua" } },
-  })
+    require("which-key").setup({
+        preset = "helix",
+        icons = { mappings = false },
+        spec = { { "gr", group = "+LSP/fzf-lua" } },
+    })
 
-  -- require("conform").setup({
-  --     formatters_by_ft = {
-  --         css = { "prettier" },
-  --         go = { "goimports", "gofmt" },
-  --         graphql = { "prettier" },
-  --         javascript = { "prettier" },
-  --         javascriptreact = { "prettier" },
-  --         json = { "prettier" },
-  --         lua = { "stylua" },
-  --         markdown = { "prettier" },
-  --         python = { "ruff_format", "ruff_organize_imports", "ruff_fix" },
-  --         sh = { "beautysh" },
-  --         zsh = { "beautysh" },
-  --         toml = { "taplo" },
-  --         terraform = { "terraform_fmt" },
-  --         typescript = { "prettier" },
-  --         typescriptreact = { "prettier" },
-  --         yaml = { "prettier" },
-  --     },
-  --     formatters = {
-  --         shfmt = { prepend_args = { "-i", "4", "-ci" } },
-  --         stylua = { prepend_args = { "--indent-type", "Spaces" } },
-  --     },
-  -- })
+    -- require("conform").setup({
+    --     formatters_by_ft = {
+    --         css = { "prettier" },
+    --         go = { "goimports", "gofmt" },
+    --         graphql = { "prettier" },
+    --         javascript = { "prettier" },
+    --         javascriptreact = { "prettier" },
+    --         json = { "prettier" },
+    --         lua = { "stylua" },
+    --         markdown = { "prettier" },
+    --         python = { "ruff_format", "ruff_organize_imports", "ruff_fix" },
+    --         sh = { "beautysh" },
+    --         zsh = { "beautysh" },
+    --         toml = { "taplo" },
+    --         terraform = { "terraform_fmt" },
+    --         typescript = { "prettier" },
+    --         typescriptreact = { "prettier" },
+    --         yaml = { "prettier" },
+    --     },
+    --     formatters = {
+    --         shfmt = { prepend_args = { "-i", "4", "-ci" } },
+    --         stylua = { prepend_args = { "--indent-type", "Spaces" } },
+    --     },
+    -- })
 
-  require("gopher").setup({ gotag = { transform = "camelcase" } })
+    require("gopher").setup({ gotag = { transform = "camelcase" } })
 
-  require("mini.files").setup()
-  require("mini.diff").setup()
-  require("mini.surround").setup()
-  -- }}}
+    require("mini.files").setup()
+    require("mini.diff").setup()
+    require("mini.surround").setup()
+    -- }}}
 
-  -- KEYMAPS {{{
-  km({ "n", "v" }, "<leader>y", '"+y', 'Copy to "+')
-  km({ "n", "v" }, "<leader>p", '"+p', 'Paste after from "+')
-  km({ "n", "v" }, "<leader>P", '"+P', 'Paste before from "+')
-  for _, v in ipairs({ { "]d", 1, "Next" }, { "[d", -1, "Previous" } }) do
-    km("n", v[1], function()
-      vim.diagnostic.jump({ count = v[2], float = { border = "bold" } })
-    end, v[3] .. " diagnostic")
-  end
-  -- km("n", "<leader>=", function() require("conform").format({
-  --     async = true, lsp_format = "fallback" }) end, "Format")
-  km("n", "<leader>=", function() vim.lsp.buf.format({ async = true }) end, "Format buffer")
-  km("n", "<leader>t", [[<cmd>%s/\s\+$//e | noh<cr>]], "Trim whitespace")
+    -- KEYMAPS {{{
+    km({ "n", "v" }, "<leader>y", '"+y', 'Copy to "+')
+    km({ "n", "v" }, "<leader>p", '"+p', 'Paste after from "+')
+    km({ "n", "v" }, "<leader>P", '"+P', 'Paste before from "+')
+    for _, v in ipairs({ { "]d", 1, "Next" }, { "[d", -1, "Previous" } }) do
+        km("n", v[1], function()
+            vim.diagnostic.jump({ count = v[2], float = { border = "bold" } })
+        end, v[3] .. " diagnostic")
+    end
+    -- km("n", "<leader>=", function() require("conform").format({
+    --     async = true, lsp_format = "fallback" }) end, "Format")
+    km("n", "<leader>=", function() vim.lsp.buf.format({ async = true }) end, "Format buffer")
+    km("n", "<leader>t", [[<cmd>%s/\s\+$//e | noh<cr>]], "Trim whitespace")
 
-  local fzf = require("fzf-lua")
-  km("n", "grr", fzf.lsp_references, "LSP references")
-  km("n", "<leader>s", fzf.lsp_document_symbols, "LSP symbols")
-  km("n", "<Leader>S", fzf.lsp_live_workspace_symbols, "LSP workspace symbols")
-  km("n", "<Leader>d", fzf.lsp_workspace_diagnostics, "Diagnostics")
-  km("n", "<Leader>h", fzf.helptags, "Help")
-  km("n", "<Leader>f", fzf.files, "Files")
-  km("n", "<Leader>/", fzf.live_grep, "Live grep")
-  km("n", "<Leader>w", fzf.grep_cword, "Grep cword")
-  km("n", "<Leader>W", fzf.grep_cWORD, "Grep cWORD")
+    local fzf = require("fzf-lua")
+    km("n", "grr", fzf.lsp_references, "LSP references")
+    km("n", "<leader>s", fzf.lsp_document_symbols, "LSP symbols")
+    km("n", "<Leader>S", fzf.lsp_live_workspace_symbols, "LSP workspace symbols")
+    km("n", "<Leader>d", fzf.lsp_workspace_diagnostics, "Diagnostics")
+    km("n", "<Leader>h", fzf.helptags, "Help")
+    km("n", "<Leader>f", fzf.files, "Files")
+    km("n", "<Leader>/", fzf.live_grep, "Live grep")
+    km("n", "<Leader>w", fzf.grep_cword, "Grep cword")
+    km("n", "<Leader>W", fzf.grep_cWORD, "Grep cWORD")
 
-  km("n", "<leader>e", require("mini.files").open, "Explorer")
-  km("n", "<leader>z", require("zen-mode").toggle, "Toggle Zen")
-  -- }}}
+    km("n", "<leader>e", require("mini.files").open, "Explorer")
+    km("n", "<leader>z", require("zen-mode").toggle, "Toggle Zen")
+    -- }}}
 end)
 -- }}}
