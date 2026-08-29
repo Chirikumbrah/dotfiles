7d9cbd3e3dc795e1ab1c7c365b2d1d03f961efbe
Chirikumbrah <dvps4k@gmail.com>
2026-08-29 19:44:50 +0300

    Neovim: reduced config lines

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,30 +1,16 @@
 vim.g.mapleader = " "
-vim.opt.cursorcolumn = true
-vim.opt.cursorline = true
-vim.opt.expandtab = true
-vim.opt.grepprg = "grep -HRIn $* ."
-vim.opt.grepformat = "%f:%l:%m"
 vim.o.formatprg = "prettier --stdin-filepath %"
-vim.opt.ignorecase = true
-vim.opt.incsearch = true
-vim.opt.list = true
-vim.opt.number = true
-vim.opt.path:append({ "**" })
-vim.opt.shiftround = true
-vim.opt.shiftwidth = 4
-vim.opt.signcolumn = "yes"
-vim.opt.smartcase = true
-vim.opt.swapfile = false
-vim.opt.smartindent = true
-vim.opt.smoothscroll = true
-vim.opt.softtabstop = 4
-vim.opt.tabstop = 4
-vim.opt.undofile = true
-vim.opt.updatetime = 50
+vim.o.signcolumn = "yes"
+vim.o.updatetime = 50
+vim.opt.path:append({ "**", "**/.*/**" })
 vim.opt.wildoptions:append({ "fuzzy" })
-vim.opt.winborder = "bold"
-vim.opt.complete = "o,.,w,b,u,t"
-vim.opt.completeopt = "menu,menuone,popup,fuzzy,noinsert"
+vim.o.winborder = "bold"
+vim.o.complete = "o,.,w,b,u,t"
+vim.o.completeopt = "menu,menuone,popup,fuzzy,noinsert"
+for _, opt in ipairs({ "shiftwidth", "tabstop", "softtabstop" }) do vim.o[opt] = 2 end
+for _, opt in ipairs({ "cursorcolumn", "cursorline", "expandtab", "ignorecase", "incsearch", "list", "number",
+  "shiftround", "smartcase", "smartindent", "smoothscroll", "undofile", }) do vim.o[opt] = true end
+vim.o.swapfile = false
 
 vim.cmd("packadd nvim.undotree | colorscheme habamax")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -32,52 +18,41 @@ vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#3f3f3f", bg = "none" })
 
 vim.filetype.add({
-    pattern = {
-        [".*/templates/.*%.tpl"] = "helm",
-        [".*/templates/.*%.ya?ml"] = "helm",
-        [".*%.ya?ml%.tmpl"] = "helm",
-        ["helmfile.*%.ya?ml"] = "helm",
-        [".*%.conf$"] = "nginx",
-        [".*%.conf%.tmpl"] = "nginx",
-    },
+  pattern = { [".*/templates/.*%.tpl"] = "helm", [".*/templates/.*%.ya?ml"] = "helm",
+    [".*%.ya?ml%.tmpl"] = "helm", ["helmfile.*%.ya?ml"] = "helm",
+    [".*%.conf$"] = "nginx", [".*%.conf%.tmpl"] = "nginx", },
 })
 
 vim.lsp.enable({ "basedpyright", "bashls", "clangd", "dockerls", "gopls", "helm_ls", "jsonls", "lua_ls", "marksman",
-    "ruff", "terraformls", "tflint", "yamlls", "taplo" })
+  "ruff", "terraformls", "tflint", "yamlls", "taplo" })
 
-vim.pack.add({
-    { src = "https://github.com/romus204/tree-sitter-manager.nvim" },
-    { src = "https://github.com/olexsmir/gopher.nvim" },
-    { src = "https://github.com/neovim/nvim-lspconfig" },
-    { src = "https://github.com/mason-org/mason.nvim" },
-    { src = "https://github.com/owallb/mason-auto-install.nvim" },
-    { src = "https://github.com/nvim-mini/mini.diff" },
-})
+vim.pack.add(vim.tbl_map(function(repo)
+  return { src = "https://github.com/" .. repo }
+end, { "romus204/tree-sitter-manager.nvim", "olexsmir/gopher.nvim", "neovim/nvim-lspconfig", "mason-org/mason.nvim",
+  "owallb/mason-auto-install.nvim", "nvim-mini/mini.diff", "nvim-mini/mini.pick",
+}))
 
-vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = 'Copy to "+' })
-vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = 'Paste after from "+' })
-vim.keymap.set({ "n", "v" }, "<leader>P", '"+P', { desc = 'Paste before from "+' })
-vim.keymap.set({ "n", "v" }, "<leader>u", vim.cmd.Undotree, { desc = "Undotree" })
-vim.keymap.set("n", "<leader>t", [[<cmd>%s/\s\+$//e | noh<cr>]], { desc = "Trim whitespace" })
-vim.keymap.set("n", "<leader>s", vim.lsp.buf.document_symbol, { desc = "LSP symbols" })
-vim.keymap.set("n", "<Leader>S", vim.lsp.buf.workspace_symbol, { desc = "Find LSP workspace symbols" })
-vim.keymap.set("n", "<Leader>d", vim.diagnostic.setqflist, { desc = "Workspace Diagnostics" })
-vim.keymap.set("n", "<Leader><leader>", ":fin <C-z>", { desc = "Find file" })
-vim.keymap.set("n", "<Leader>/", ":copen | :silent :grep ", { desc = "Grep" })
-vim.keymap.set("n", "<Leader>g", require("mini.diff").toggle_overlay, { desc = "Show Diff" })
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
+vim.keymap.set({ "n", "v" }, "<leader>P", '"+P')
+vim.keymap.set({ "n", "v" }, "<leader>u", vim.cmd.Undotree, { silent = true })
+vim.keymap.set("n", "<leader>t", [[<cmd>%s/\s\+$//e | noh<cr>]], { silent = true })
+vim.keymap.set("n", "<leader>s", vim.lsp.buf.document_symbol, { silent = true })
+vim.keymap.set("n", "<leader>S", vim.lsp.buf.workspace_symbol, { silent = true })
+vim.keymap.set("n", "<leader>d", vim.diagnostic.setqflist, { silent = true })
+vim.keymap.set("n", "<leader>g", require("mini.diff").toggle_overlay, { silent = true })
+vim.keymap.set("n", "<leader>/", ":Pick grep_live<cr>", { silent = true })
+vim.keymap.set("n", "<leader><leader>", ":Pick files<cr>", { silent = true })
 
 vim.api.nvim_create_autocmd("TextYankPost", { callback = function() vim.hl.on_yank({ timeout = 400 }) end, })
-
 vim.api.nvim_create_autocmd("FileType", {
-    callback = function()
-        if vim.bo.filetype == "go" then require("gopher").setup({ gotag = { transform = "camelcase" } }) end
-    end
+  callback = function()
+    if vim.bo.filetype == "go" then require("gopher").setup({ gotag = { transform = "camelcase" } }) end
+  end
 })
 
 require("tree-sitter-manager").setup({ auto_install = true, ensure_installed = { "gotmpl" } })
-require("mini.diff").setup()
-require("mason").setup()
+for _, mod in ipairs({ "mini.diff", "mini.pick", "mason" }) do require(mod).setup() end
 require("mason-auto-install").setup { packages = { "basedpyright", "bash-language-server", "clangd",
-    "docker-language-server", "json-lsp", "gopls", "helm-ls", "shellcheck", "yaml-language-server",
-    "lua-language-server", "marksman", "prettier", "ruff", "taplo", "terraform-ls", "tflint", },
+  "docker-language-server", "json-lsp", "gopls", "helm-ls", "yaml-language-server",
+  "lua-language-server", "marksman", "prettier", "ruff", "taplo", "terraform-ls", "tflint", },
 }
